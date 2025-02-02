d76c8b68c4a9cf74fbf8210afe8f9137d1adf93e
Hugo Vinícius <158780019+hugovini1@users.noreply.github.com>
2025-02-02 16:39:12 -0300

    BeeCrowd_Problem1007

---
+++ 1007.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+    int a, b, c, d, diferenca=0;
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+    
+    diferenca = (a*b - c*d);
+
+    printf("DIFERENCA = %d\n", diferenca);
+
+    return 0;
+}
